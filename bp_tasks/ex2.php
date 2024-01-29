f6b77e04f1331aaef7f2d1d787cecb5ccf970428
BibekPandey7 <146955877+BibekPandey7@users.noreply.github.com>
2024-01-29 15:47:43 +0200

    exercise2 done

---
+++ bp_tasks/ex2.php
@@ -0,0 +1,104 @@
+<?php include 'header.php' ?>
+<link rel="stylesheet" href="style1.css">
+
+<div class="main-menu">
+        <div class="menu">
+        <h2><a href="#" class="btn btn-danger" >Momo</a></h2>
+        
+            <h3>Veg momo</h3>
+            <img src="image/veg-momo.jpg" >
+        
+            <div class="more">
+                <p>
+            Large : €10.56 <br>
+                    Medium: €8.99 <br>
+                    Small :  €5.95 
+                </p>
+            </div>
+            <h3>chicken momo</h3>
+            <img src="image/chikenmomos.jpg" >
+            <div class="more">
+                <p>
+                    Large : €12.65 <br>
+                    Medium: €9.64 <br>
+                    Small :  €7.95 
+                </p>
+            </div>
+        </div>
+        <div class="menu">
+ <h2><a href="#" class="btn btn-danger" >Biryani</a></h2>
+ 
+    <h3>Chicken Biryani</h3>
+    <img src="image/chicken biryani.jpg">
+
+    <p>
+      
+        Large: €11.23 <br>
+        Medium: €8.98 <br>
+        Small:  €7.25 
+    </p>
+    <h3>Egg Biryani</h3>
+       <img src="image/eggbiryanii.jpg">
+
+       <p>
+        Large: €12.88 <br>
+        Medium: €9.65 <br>
+        Small: €6.88 
+    </p>
+
+</div>
+
+<div class="menu">
+    <h2><a href="#" class="btn btn-danger" >Beverages</a></h2>
+    <h3>Tea</h3>
+        <img src="image/Tea.jpg">
+        <p>
+            Black tea: €6<br>
+            White tea: €8
+    
+    </p>
+
+    <h3>Coffee</h3>
+        <img src="image/coffeee.jpg">
+        <p>
+            Big coffee: €4<br>
+            Small coffee: €2
+        </p>
+
+    </div>
+    <div class="menu">
+    <h2><a href="#" class="btn btn-danger" >Local Foods</a></h2>
+    <h3>Selroti</h3>
+        <img src="image/selrotii.jpg" > <p>
+            Large: €12.99</br>
+            Medium: €9.85<br>
+            Small: €5.75
+        </p>
+    <h3>Thakali khana Set</h3>
+        <img src="image/Thakali-Khana-Set.jpg">
+        <p>
+            Large: €11.78<br>
+            Medium: €9.85<br>
+            Small: €6.85
+        </p>
+        <h3>Paneer Roti Set</h3>
+        <img src="image/Paneer roti.jpg">
+        <p>
+            Large: €10.45<br>
+            Medium: €8.65<br>
+            Small: €6.85
+        </p>
+       
+    </div>
+    </div>
+
+
+</div>
+</body>
+        </div>
+    <header>
+
+</header>
+</article>
+
+<?php include 'footer.php' ?>
