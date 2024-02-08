f7985ad78fd279e423fd4a2126f229df63183506
BibekPandey7 <146955877+BibekPandey7@users.noreply.github.com>
2024-02-08 03:32:35 +0200

    Done

---
+++ ex7.php
@@ -0,0 +1,13 @@
+<?php
+include 'header.php' ;
+?>
+<body>
+    <h1>click the option here</h1>
+<nav>
+    <a href="./crud/index.php">Enter Data</a>
+    <a href="./crud/read.php">Read Data</a>
+    </nav>
+</body>
+<?php
+include 'footer.php';
+?>
